b966d612b61ac7892496f59c9ca11f5263754895
Ahmed Naseem Qureshi <ahmednq.dev@gmail.com>
2024-10-31 15:24:39 +0500

    Add files via upload

---
+++ Resume-Builder/Milestone-1/script.ts
@@ -0,0 +1,24 @@
+
+let opene = document.getElementById('opener') as HTMLDivElement
+let main = document.getElementById('main') as HTMLDivElement
+
+setTimeout(() => {
+    opene.style.display = 'none' ;
+    main.style.display = 'block';
+}, 7000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
